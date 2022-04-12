0a2ab3150d96d6449560d77ad681d05d059640bb
rohithb4560 <rbjuly31@gmail.com>
2022-04-12 11:47:15 +0530

    firstpartdone

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/info', function(req, res, next) {
+  res.status(200).json({data: "Welcome to Actyv!!"})
+}); 
+
 module.exports = router;
