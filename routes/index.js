6f5f7c9ed124f8f55b56a1910f9da126d37971b5
rohithb4560 <rbjuly31@gmail.com>
2022-04-12 12:21:10 +0530

    day2_task_completed

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,10 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  res.render('login',{username:"Rohith",password:"actyv"})
 });
-
+router.get('/auth/login', function(req, res, next) {
+  
+  res.render('login',{username:"Rohith",password:"actyv"})
+});
 module.exports = router;
